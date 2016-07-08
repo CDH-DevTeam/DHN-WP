05ede4909e985c9de675cf8924f9372f8786558e
traustid <traustid@gmail.com>
2016-07-08 23:07:59 +0200

    Added Google Analytics

--- wp-content/themes/dhn/footer.php
+++ wp-content/themes/dhn/footer.php
@@ -19,5 +19,16 @@
 <script src="https://npmcdn.com/masonry-layout@4.1/dist/masonry.pkgd.min.js"></script>
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/site.js"></script>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-80476789-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 </body>
 </html>
