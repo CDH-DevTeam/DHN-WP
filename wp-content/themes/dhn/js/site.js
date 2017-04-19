4fffe4ef6083121a323010548ade7cafdba3d47f
traustid <traustid@gmail.com>
2017-04-19 21:21:29 +0200

    Added imageLoaded masonry event handler.

--- wp-content/themes/dhn/js/site.js
+++ wp-content/themes/dhn/js/site.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 	$('.tile-boxes>ul>li').wrapInner('<div class="inner"></div>');
 
-	$('.tile-boxes>ul').masonry({
+	var $ulGrid = $('.tile-boxes>ul').masonry({
 		itemSelector: 'li',
 		percentPosition: true
 	});
 
-	$('.tile-grid').masonry({
+	$ulGrid.imagesLoaded().progress( function() {
+		$ulGrid.masonry('layout');
+	});
+
+	var $grid = $('.tile-grid').masonry({
 		itemSelector: '.grid-item',
 		percentPosition: true
+	});
+
+	$grid.imagesLoaded().progress( function() {
+		$grid.masonry('layout');
 	});
 
 	$('.menu-button').click(function() {
